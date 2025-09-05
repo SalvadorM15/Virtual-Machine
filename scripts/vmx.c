5b526ea0fd03a98f167e4361b533d3da58e9e840
Fede Calla <fedecalla975@gmail.com>
2025-09-05 17:46:34 -0300

    creacion_de_funcion_logical_to_physical

--- scripts/vmx.c
+++ scripts/vmx.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+
+//CONSTANTES AUXILIARES QUE CREE PARA TESTEAR LA FUNCION
+const MAX = 2;
+const seg_table[2][2] = {0, 0x0033, 0x0033, 0xFFCD};
+
 void main(){
     printf("hola mundo/n");
+}
+
+
+int logical_to_physical(int logical_dir ,int seg_table[MAX][2], int cant_bytes){
+    int physical_dir;
+    int segment = (logical_dir & 0xFFFF0000) >> 16;
+    int segment_limit = seg_table[segment][0] + seg_table[segment][1];
+
+    if(segment < MAX){
+        physical_dir = seg_table[segment][0];
+        physical_dir += (logical_dir & 0x0000FFFF);
+        if(physical_dir > segment_limit || physical_dir + cant_bytes > segment_limit)
+            physical_dir = -1;
+    }
+    else
+        physical_dir = -1;
+
+
+    return physical_dir;
 }
